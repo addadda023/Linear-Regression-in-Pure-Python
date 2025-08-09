ba3fdca5c627143fe366eba32b75d6ce3023b121
addadda023 <avsardahal@gmail.com>
2025-08-09 13:29:32 -0700

    Create micro_grad.py

---
+++ micro_grad.py
@@ -0,0 +1,81 @@
+"""Adapted from micrograd/engine.py"""
+
+from typing import Tuple, Self
+
+class Value:
+    def __init__(self, value: float, _children: Tuple[Self, Self]=(), _op: str='', label: str=''):
+        self.value = value
+        self.grad = 0.0
+        self._backward = lambda: None
+        self.children = set(_children)
+        self.op = _op
+        self.label = label
+    
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
+    
+    def __add__(self, other) -> Self:
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.value + other.value, (self, other), '+')
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+        return out
+    
+    def __mul__(self, other) -> Self:
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.value * other.value, (self, other), '*')
+
+        def _backward():
+            self.grad += out.grad * other.value
+            other.grad += out.grad * self.value
+        out._backward = _backward
+        
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.value**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.value**(other - 1)) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v.children:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __radd__(self, other): # other + self
+        return self + otherg
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
