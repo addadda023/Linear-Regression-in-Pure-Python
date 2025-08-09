57d29510ac0207892c25bb549d718cf07c1f018c
addadda023 <avsardahal@gmail.com>
2025-08-09 13:38:20 -0700

    Update linear_reg_from_first_principles.py
    Make the batch and labels same in both linear regression approaches.

--- linear_reg_from_first_principles.py
+++ linear_reg_from_first_principles.py
@@ -80,17 +80,19 @@ class LinearRegression():
 class TestLinearRegression(unittest.TestCase):
 
     def setUp(self):
-        self.model = LinearRegression(batch_size=2, num_features=4)
-        self.features_batch = [[1.0, 2.0, 1.0, 4.0], [1.0, 2.0, 1.0, 4.0]]
-        self.labels = [-0.402, -0.402]
+        self.model = LinearRegression(batch_size=5, num_features=2)
+        self.features_batch = [[1.0, 2.0], [2.0, 3.0], [3.0, 4.0], [4.0, 5.0], [5.0, 6.0]]
+        self.labels = [3.0, 5.0, 7.0, 9.0, 11.0]
+        # self.features_batch = [[1.0, 2.0, 1.0, 4.0], [1.0, 2.0, 1.0, 4.0]]
+        # self.labels = [-0.402, -0.402]
     
     def test_get_weights_before_training(self):
         model_weights = self.model.weights
-        self.assertEqual(len(model_weights), 4)
+        self.assertEqual(len(model_weights), 2)
 
     def test_forward_pass(self):
         predictions = self.model.forward(self.features_batch)
-        self.assertEqual(len(predictions), 2)
+        self.assertEqual(len(predictions), 5)
         self.assertEqual(predictions[0], predictions[1])
 
     def test_successful_back_prop(self):
@@ -106,8 +108,8 @@ class TestLinearRegression(unittest.TestCase):
         predictions = [round(_,4) for _ in self.model.forward(self.features_batch)]
         print(f"Before training.\nPrediction: {predictions}")
         print("Starting training.")
-        for index in range(150):
-            pred, loss = self.model.train(features=self.features_batch, labels=self.labels, learning_rate=0.005)
+        for index in range(200):
+            pred, loss = self.model.train(features=self.features_batch, labels=self.labels, learning_rate=0.0005)
             pred = [round(_,4) for _ in pred]
             print(f'Step {index}. Predictions: {pred}. Loss: {loss:5f}')
             
