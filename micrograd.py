d7e61fdd022c491784f714b86b06cccf8d7a828b
addadda023 <avsardahal@gmail.com>
2025-08-15 21:25:59 -0700

    Update micrograd.py
    Add Momentum and Adam optimizer.

--- micrograd.py
+++ micrograd.py
@@ -1,4 +1,4 @@
-"""Micrograd with support for custom optimizers."""
+"""Extend micrograd to use custom optimizers."""
 
 import math
 from typing import Tuple, Self
@@ -110,3 +110,33 @@ class SGDOptimizer(Optimizer):
     def step(self):
         for p in self.parameters:
             p.value -= self.learning_rate * p.grad
+
+class MomentumOptimizer(Optimizer):
+    def __init__(self, parameters, learning_rate=0.01, momentum=0.9):
+        super().__init__(parameters, learning_rate)
+        self.momentum = momentum
+        self.velocity = {p: 0 for p in self.parameters}
+
+    def step(self):
+        for p in self.parameters:
+            self.velocity[p] = self.momentum * self.velocity[p] + p.grad
+            p.value -= self.learning_rate * self.velocity[p]
+
+class AdamOptimizer(Optimizer):
+    def __init__(self, parameters, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        super().__init__(parameters, learning_rate)
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = {p: 0 for p in self.parameters}
+        self.v = {p: 0 for p in self.parameters}
+        self.t = 0
+
+    def step(self):
+        self.t += 1
+        for p in self.parameters:
+            self.m[p] = self.beta1 * self.m[p] + (1 - self.beta1) * p.grad
+            self.v[p] = self.beta2 * self.v[p] + (1 - self.beta2) * (p.grad**2)
+            m_hat = self.m[p] / (1 - self.beta1**self.t)
+            v_hat = self.v[p] / (1 - self.beta2**self.t)
+            p.value -= self.learning_rate * m_hat / (math.sqrt(v_hat) + self.epsilon)
